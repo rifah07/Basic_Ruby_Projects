5fe8a63e0586f0ec5efdfef473d4dd5fcc254fdd
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-06 16:35:38 +0600

    now it also takes input from user

--- ceser_cipher/caesar_cipher.rb
+++ ceser_cipher/caesar_cipher.rb
@@ -13,3 +13,9 @@ end
 
 puts caesar_cipher("What a string!", 5)
 puts caesar_cipher("Khoor, Zruog!", -3)
+
+print "Enter the message you want to encrypt: "
+user_message = gets.chomp
+print "Enter the amount of shifts: "
+shift_amount = gets.chomp.to_i
+puts caesar_cipher(user_message, shift_amount) # ruby caesar_cipher.rb in terminal
