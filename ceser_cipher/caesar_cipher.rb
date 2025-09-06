9b5066858c295905dc37d7d0c8a9ebd8aaa88ab6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-06 16:28:43 +0600

    slightly updated

--- ceser_cipher/caesar_cipher.rb
+++ ceser_cipher/caesar_cipher.rb
@@ -1,15 +1,15 @@
 def caesar_cipher(text, shift)
-  text.each_char do |char|
+  shift %= 26
+  result = text.each_char.map do |char|
     if char =~ /[A-Za-z]/
       base = char.ord < 91 ? 65 : 97
-      encrypted_char = ((char.ord-base+shift)%26 + base).chr
-      print encrypted_char
+      ((char.ord-base+shift)%26 + base).chr
     else
-      print char
+      char
     end
   end
-  puts ""
+  result.join
 end
 
-caesar_cipher("What a string!", 5)
-caesar_cipher("Khoor, Zruog!", -3)
+puts caesar_cipher("What a string!", 5)
+puts caesar_cipher("Khoor, Zruog!", -3)
