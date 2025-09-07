ae2ed96ca75a07197c77b6af488452e5ef5875c6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-07 19:28:34 +0600

    it works but does not check substring from a 'word' string

--- substring.rb
+++ substring.rb
@@ -1,11 +1,17 @@
 def substrings (str, dictionary)
   #puts str
   #puts dictionary
-  str_ar = str.split # string to array coverted
-  puts str_ar
+  str_ar = str.downcase.split # string to array coverted
+  #puts str_ar
+  count = Hash.new(0)
+  str_ar.each do |word|
+    if dictionary.include?(word)
+        count[word] += 1
+    end
+  end
 
-
+  count
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
