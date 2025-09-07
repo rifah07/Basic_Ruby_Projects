995f92441bd0f6d61c2dc107c72fe6c7da5eb5a7
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-07 19:08:51 +0600

    for substring method srting to array converted

---
+++ substring.rb
@@ -0,0 +1,11 @@
+def substrings (str, dictionary)
+  #puts str
+  #puts dictionary
+  str_ar = str.split # string to array coverted
+  puts str_ar
+
+
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("Howdy partner, sit down! How's it going?", dictionary)
