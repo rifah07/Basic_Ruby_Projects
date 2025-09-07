54f7400457220b290ea695c5da97b243dbfe437b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-07 19:51:41 +0600

    slightly updated but not done

--- substring.rb
+++ substring.rb
@@ -1,16 +1,9 @@
 def substrings (str, dictionary)
-  #puts str
-  #puts dictionary
-  str_ar = str.downcase.split # string to array coverted
+  str_ar = str.downcase.scan(/\w+/) # string to array coverted
   #puts str_ar
-  count = Hash.new(0)
-  str_ar.each do |word|
-    if dictionary.include?(word)
-        count[word] += 1
-    end
+  str_ar.each_with_object(Hash.new(0)) do |word, counts|
+    counts[word] += 1 if dictionary.include?(word)
   end
-
-  count
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
