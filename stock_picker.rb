dcf05019f01ac44c22640a3f25ce53fb60de2d51
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-08 21:11:22 +0600

    updated

--- stock_picker.rb
+++ stock_picker.rb
@@ -17,7 +17,9 @@ def stock_picker(prices)
       buy_day = temp_buy_day
     end
   end
-  [buy_day, sell_day]
+
+ [buy_day, sell_day]
 end
 
 puts stock_picker([17,3,6,9,15,8,6,1,10])
+#puts stock_picker([17,3,6,9,15,8,6,1,10,20])
