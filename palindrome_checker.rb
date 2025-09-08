fb870a46b9ba831952e27a61717ef0c016970398
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-08 22:30:44 +0600

    more simplified palindrome checker

--- palindrome_checker.rb
+++ palindrome_checker.rb
@@ -1,11 +1,9 @@
 def palindrome_checker(word)
-  if(word.reverse == word)
-    return true
-  else
-    return false
-  end
-
+  word = word.to_s
+  word.reverse == word
 end
 
 puts palindrome_checker("Bizzare")
 puts palindrome_checker("madam")
+puts palindrome_checker(11211)
+puts palindrome_checker(112112)
