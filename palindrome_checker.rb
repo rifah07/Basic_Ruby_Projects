7ba797c842fb2fc2246af33a2a8a01831acb2afe
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-09 11:17:08 +0600

    explaination added

--- palindrome_checker.rb
+++ palindrome_checker.rb
@@ -19,3 +19,17 @@ puts palindrome_checker("Bizzar")
 puts palindrome_checker("madam")
 puts palindrome_checker(11211)
 puts palindrome_checker(112112)
+
+=begin 
+
+Previous solution :
+  word = word.to_s
+  #word.reverse == word
+  - Time complexity: O(n)
+  - Space complexity: O(n) [as it creates a new reversed string]
+  
+  Current solution is given above
+  - Time complexity: O(n) [actually O(n/2) but we drop the constant factor so /2 is removed]
+  - Space complexity: O(1)
+
+=end
