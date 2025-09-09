65330b651fd8264b7317a20c3f72b89cc482e6b1
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-09 11:11:08 +0600

    without reverse method palindrome implemented

--- palindrome_checker.rb
+++ palindrome_checker.rb
@@ -1,9 +1,21 @@
 def palindrome_checker(word)
   word = word.to_s
-  word.reverse == word
+  #word.reverse == word
+  n = word.length
+  left = 0
+  right = n - 1
+  while (left < right)
+    if(word[left] == word[right])
+      left += 1
+      right -= 1
+    else
+      return false
+    end
+  end
+  true
 end
 
-puts palindrome_checker("Bizzare")
+puts palindrome_checker("Bizzar")
 puts palindrome_checker("madam")
 puts palindrome_checker(11211)
 puts palindrome_checker(112112)
