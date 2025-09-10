ac0ec03891f64c03e761b88f503b0e6d888cb579
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-09-10 14:53:04 +0600

    fizzBuzz problem ans solution updated

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,16 +1,26 @@
 =begin 
-Print numbers 1 to 100 with multiples of 3 replaced by "Fizz",
-multiples of 5 replaced by "Buzz", and multiples of both replaced by "FizzBuzz".
+Given an integer n, return a string array answer (1-indexed) where:
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
 =end
 
-(1..100).each do |n|
-  if((n % 3 == 0) && (n % 5 == 0))
-    p "FizzBuzz"
-  elsif(n % 3 == 0)
-    p "Fizz"
-  elsif(n % 5 == 0)
-    p "Buzz"
-  else
-    p n
-  end
+# @param {Integer} n
+# @return {String[]}
+def fizz_buzz(n)
+    answer = []
+
+    (1..n).each do |i|
+        if ((i % 3 == 0) && (i % 5 == 0))
+         answer[i] = "FizzBuzz"
+        elsif (i % 3 == 0)
+         answer[i] = "Fizz"
+        elsif (i % 5 == 0)
+         answer[i] = "Buzz"
+        else
+         answer[i] = i.to_s
+        end
+    end
+    answer[1..-1]  
 end
