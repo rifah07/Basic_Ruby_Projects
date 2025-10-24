2e7a864fbd12f12d455c768b516a025e96afe6ac
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-10-24 20:15:45 +0600

    book class added

--- library_management/book.rb
+++ library_management/book.rb
@@ -1,4 +1,27 @@
-# frozen_string_literal: true
+=begin
+Book class should have:
+ - Attributes: title, author, isbn, availability status
+ - Methods: to_s for string representation, available? predicate method
+ - Consider adding: genre, publication_year, checked_out_by, due_date
+=end
 
 class Book
+  attr_accessor :availability_status
+  attr_reader :title, :author, :year, :isbn
+
+  def initialize(title, author, isbn, year)
+    @title = title
+    @author = author
+    @isbn = isbn
+    @year = year
+    @availability_status = :available
+  end
+
+  def to_s
+    "#{title} by #{author} (#{year}) - ISBN: #{isbn}"
+  end
+
+  def available?
+    @availability_status == :available
+  end
 end
