0ee1ba1ea98f8d3bb857c25245d70798c28dcee0
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-10-26 22:01:51 +0600

    member instance created

--- library_management/main.rb
+++ library_management/main.rb
@@ -5,6 +5,10 @@ require_relative 'book'
 require_relative 'library'
 require_relative 'member'
 
+# member class
+member = Member.new('Rifah', 101)
+puts member
+
 book = Book.new('1984', 'George Orwell', '978-0451524935', 'Dystopian', 1949)
 puts book                    # 1984 by George Orwell (1949) - ISBN: 978-0451524935
 puts book.available?         # true
@@ -18,3 +22,9 @@ book.due_date = Date.today + 14
 
 puts book.available?         # false
 puts book.checked_out?       # true
+puts ""
+puts member.has_book?(book)
+puts member.checkout_book(book)
+puts member.can_checkout?
+puts member.return_book(book)
+puts member.can_checkout?
